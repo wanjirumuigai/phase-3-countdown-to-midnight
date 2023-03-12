120c58e2ca2492a3bf1e5e6149033db94f711083
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-12 10:04:51 +0300

    completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+#write your code h
 
-def countdown
+def countdown(number)
+while number > 0
+    puts "#{number} SECOND(S)!"
+    
+    number -=1
 end
+"HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        sleep 5
+        number -=1
+    end
+    "HAPPY NEW YEAR!"
+    end
+    countdown_with_sleep(4)
